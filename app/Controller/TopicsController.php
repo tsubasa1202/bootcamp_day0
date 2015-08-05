31698beb53b8cfa8983f16c3bd68d4a2e06533f2
岩崎拓海 <sky@iwasakitakumi-no-MacBook-Air.local>
2015-08-05 19:53:59 +0900

    firstcommit

---
+++ app/Controller/TopicsController.php
@@ -0,0 +1,25 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class TopicsController extends AppController {
+
+    public function index(){
+
+	}
+
+	public function view() {
+
+	}
+
+	public function add() {
+		
+	}
+
+	public function edit() {
+
+	}
+
+	public function delete() {
+
+	}
+}
---
+++ app/View/Layouts/default.ctp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+</head>
+<body>
+	<div class="container-fluid">
+		<?php echo $this->fetch('content'); ?>
+	</div>
+</body>
+</html>
